60b67df8adfe27ea21a44e76d1fd6adf76bc7872
wcholewinski <cholewinskii@gmail.com>
2018-06-09 14:50:15 +0200

    Initial commit

---
+++ src/main/java/com/sda/spring/bikeRent/BikeRentApplication.java
@@ -0,0 +1,12 @@
+package com.sda.spring.bikeRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRentApplication.class, args);
+	}
+}
