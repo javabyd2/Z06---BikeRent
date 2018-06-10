dc61538a17cc2b5c635fc5d37c2d473d44b6da54
Krystian Niemczewski <krystianiemczewski@gmailcom>
2018-06-10 14:17:12 +0200

    add register_template

--- src/main/java/com/sda/spring/bikeRent/repository/RentRepository.java
+++ src/main/java/com/sda/spring/bikeRent/repository/RentRepository.java
@@ -1,8 +1,7 @@
 package com.sda.spring.bikeRent.repository;
 
+import com.sda.spring.bikeRent.model.Rent;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RentRepository extends CrudRepository<Rent,Long> {
-
-
 }
