6e0e1ee67c5c6bf03f06d2905870b7d0aa336252
Krystian Niemczewski <krystianiemczewski@gmailcom>
2018-06-16 17:24:36 +0200

    editUser template and exception classes complete

---
+++ src/main/java/com/sda/spring/bikeRent/exceptions/BikeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sda.spring.bikeRent.exceptions;
+
+public class BikeNotFoundException extends RuntimeException {
+    public BikeNotFoundException(Long id) {super("Nie moge znalezc roweru"+id);}
+
+
+}
