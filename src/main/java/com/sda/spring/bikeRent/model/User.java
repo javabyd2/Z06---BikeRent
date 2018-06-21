a7ea1cb5cdd1a97ad506d4b48a8059345c65107c
wcholewinski <cholewinskii@gmail.com>
2018-06-21 22:08:45 +0200

    Waldemar Cholewinski - added @AllArgsConstructor; @NoArgsConstructor

--- src/main/java/com/sda/spring/bikeRent/model/User.java
+++ src/main/java/com/sda/spring/bikeRent/model/User.java
@@ -1,32 +1,24 @@
 package com.sda.spring.bikeRent.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String userName;
     private String userLastName;
     private String email;
     private String password;
-
-    public User() {
-    }
-
-    public User(String userName, String userLastName, String email, String password) {
-        this.userName = userName;
-        this.userLastName = userLastName;
-        this.email = email;
-        this.password = password;
-    }
-
 }
