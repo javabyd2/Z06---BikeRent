08d4c475992f1c56ee2ad202d062df22fb35baad
wcholewinski <cholewinskii@gmail.com>
2018-06-21 22:05:37 +0200

    Waldemar Cholewinski - formatted class

--- src/main/java/com/sda/spring/bikeRent/exceptions/PlaceNotFoundException.java
+++ src/main/java/com/sda/spring/bikeRent/exceptions/PlaceNotFoundException.java
@@ -1,5 +1,5 @@
 package com.sda.spring.bikeRent.exceptions;
 
 public class PlaceNotFoundException extends RuntimeException {
-    public PlaceNotFoundException(Long id) {super("Nie moge znalezc miejsca" +id);}
+    public PlaceNotFoundException(Long id) {super("Nie moge znalezc miejsca: " +id);}
 }
