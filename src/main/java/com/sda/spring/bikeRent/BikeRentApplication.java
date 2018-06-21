424592e05fcd33c93b1cd1df4f54beb55bed3648
wcholewinski <cholewinskii@gmail.com>
2018-06-21 22:02:15 +0200

    Waldemar Cholewinski - added @EnableWebMvc annotation

--- src/main/java/com/sda/spring/bikeRent/BikeRentApplication.java
+++ src/main/java/com/sda/spring/bikeRent/BikeRentApplication.java
@@ -3,11 +3,12 @@ package com.sda.spring.bikeRent;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class BikeRentApplication {
-
-
+@EnableWebMvc
+public class BikeRentApplication extends WebMvcAutoConfiguration {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BikeRentApplication.class, args);
