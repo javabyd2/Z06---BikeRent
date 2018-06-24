291a6c4e6f7a4ed686960e6baa61e2f46836d09f
wcholewinski <cholewinskii@gmail.com>
2018-06-24 10:29:10 +0200

    Waldemar Cholewinski - Controller changed name from LoginController

--- src/main/java/com/sda/spring/bikeRent/controller/Controller.java
+++ src/main/java/com/sda/spring/bikeRent/controller/Controller.java
@@ -1,19 +1,50 @@
 package com.sda.spring.bikeRent.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.sda.spring.bikeRent.model.User;
+import com.sda.spring.bikeRent.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-public class Controller {
+public class Controler {
 
-    @GetMapping(value = "/index")
-    public ModelAndView index() {
+    @Autowired
+    private UserService userService;
 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView registration() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ModelAndView AddNewUser(User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExist = userService.findUserByEmail(user.getEmail());
+        if (userExist != null) {
+            bindingResult.rejectValue("email", "error.user", "User with provided email already exist");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("registration");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "You registered correctly");
+            modelAndView.addObject("User", new User());
+            modelAndView.setViewName("registration");
+        }
         return modelAndView;
     }
 }
