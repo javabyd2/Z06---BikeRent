7ed567fd7f10430570f5782db135135e3bc16d59
wcholewinski <cholewinskii@gmail.com>
2018-06-24 23:30:41 +0200

    Waldemar Cholewinski - added annotations

--- src/main/java/com/sda/spring/bikeRent/model/User.java
+++ src/main/java/com/sda/spring/bikeRent/model/User.java
@@ -1,24 +1,34 @@
 package com.sda.spring.bikeRent.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.Set;
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
     private Long id;
+    @Column(name = "userName")
     private String userName;
+    @Column(name = "userLastName")
     private String userLastName;
+    @Column(name = "email")
     private String email;
+    @Column(name = "password")
     private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
 }
