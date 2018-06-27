4cdf88a71342c1328b2083cce8676cc747769c8e
wcholewinski <cholewinskii@gmail.com>
2018-06-27 23:07:44 +0200

    Waldemar Cholewinski - update permit options

--- src/main/java/com/sda/spring/bikeRent/config/SecurityConfig.java
+++ src/main/java/com/sda/spring/bikeRent/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers("/", "/signup", "/login").permitAll()
+                .antMatchers("/", "/signup", "/login","/addBike").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/login").failureUrl("/login?error=true")
